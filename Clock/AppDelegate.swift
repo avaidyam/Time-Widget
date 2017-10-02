ee33fc807580c127c3661533ad753b73236d7980
Aditya Vaidyam <avaidyam@purdue.edu>
2017-10-02 11:11:22 -0400

    Update for High Sierra and Swift 4.

--- Clock/AppDelegate.swift
+++ Clock/AppDelegate.swift
@@ -1,7 +1,11 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate {}
 
+class WindowController: NSWindowController {
+    override func windowDidLoad() {
+        self.window?.standardWindowButton(.closeButton)?.superview?.isHidden = true
+        self.window?.level = .floating
+    }
 }
-
--- Time/TodayViewController.swift
+++ Time/TodayViewController.swift
@@ -1,37 +1,52 @@
 import Cocoa
 import NotificationCenter
 
-class TodayViewController: NSViewController, NCWidgetProviding {
+public class TodayViewController: NSViewController, NCWidgetProviding {
     
-    var timer = NSTimer()
+    private lazy var timer: DispatchSourceTimer = {
+        let timer = DispatchSource.makeTimerSource(flags: [], queue: DispatchQueue.main)
+        timer.schedule(wallDeadline: .now() + Date().nearestMinute().timeIntervalSinceNow,
+                                 repeating: 60.0, leeway: .seconds(3))
+        timer.setEventHandler { [weak self] in
+            self?.tick()
+        }
+        return timer
+    }()
     
     @IBOutlet var clock: NSTextField!
     @IBOutlet var aux: NSTextField!
     
-    override var nibName: String? {
-        return "TodayViewController"
+    public override var nibName: NSNib.Name? {
+        return NSNib.Name(rawValue: "TodayViewController")
     }
     
-    var widgetAllowsEditing: Bool {
+    public var widgetAllowsEditing: Bool {
         return false
     }
     
-    override func viewDidAppear() {
+    public override func viewDidAppear() {
         super.viewDidAppear()
-        
+        self.timer.resume()
         tick()
-        self.timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self,
-            selector: Selector("tick"), userInfo: nil, repeats: true)
     }
     
-    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
-        tick();
-        completionHandler(.NewData)
+    public func widgetPerformUpdate(completionHandler: @escaping (NCUpdateResult) -> Void) {
+        tick()
+        completionHandler(.newData)
     }
     
     @objc func tick() {
-        clock.stringValue = NSDateFormatter.localizedStringFromDate(NSDate(),
-                            dateStyle: .NoStyle, timeStyle: .ShortStyle)
-        aux.stringValue = NSTimeZone.localTimeZone().abbreviation!
+        self.clock.stringValue = DateFormatter.localizedString(from: Date(),
+                                                               dateStyle: .none, timeStyle: .short)
+        self.aux.stringValue = TimeZone.current.abbreviation()!
+    }
+}
+
+public extension Date {
+    public func nearestMinute() -> Date {
+        let c = Calendar.current
+        var next = c.dateComponents(Set<Calendar.Component>([.minute]), from: self)
+        next.minute = (next.minute ?? -1) + 1
+        return c.nextDate(after: self, matching: next, matchingPolicy: .strict) ?? self
     }
 }
